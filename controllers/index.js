c90bc49bd6516b93c73437ae342a73abb1b5b8ee
Henok Alemu Addis <henok4@gmail.com>
2015-02-04 06:45:08 +0000

    trying out things

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,18 @@
 'use strict';
+var events = require('../models/events');
 
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var html = "";
+   for(var i=0; i< events.all.length; i++){
+        html = html + events.all[i].title + "<br>";
+    }
   var contextData = {
     'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'tagline': 'You are doomed (just kidding).',
+    'html': html
   };
   response.render('index.html', contextData);
 }
